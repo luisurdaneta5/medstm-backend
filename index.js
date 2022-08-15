fd97b66b9df70e298e274028eff79ca7b64ed840
Luis Urdaneta <luis.urdaneta488@yahoo.com.ve>
2022-08-14 22:37:36 -0400

    Index update

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(
 );
 app.use(bodyParser.json());
 app.use(express.static("public"));
-app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static("public"));
 app.use(express.json());
 app.use(
 	fileUpload({
